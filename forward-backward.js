433cf8b12fca5711427d1566cd526de8edf7fa10
sid-120 <siddharthuzumaki2000@gmail.com>
2025-04-26 15:25:46 +0530

    2. Forward Backward Exercise

---
+++ forward-backward.js
@@ -0,0 +1,18 @@
+/* Chapter 2| Arrays
+Q2. Store a set of words in an array and display the contents both forward and backward. */
+
+const words = [
+  "the",
+  "quick",
+  "brown",
+  "fox",
+  "jumps",
+  "over",
+  "the",
+  "lazy",
+  "dog",
+];
+//displaying array elements forward
+console.log("Forward display of content of an array: " + words);
+//displaying array elements backward
+console.log("Backward display of content of an array: " + words.reverse());
